a4e62803599f9b66dad1a131c92f361f3b58b8a3
Paul Féry <p.h.fery-1@student.tudelft.nl>
2022-05-11 17:07:02 +0200

    data mover functional (can work with different file types for the annotations)

--- code/datasets/data_mover.py
+++ code/datasets/data_mover.py
@@ -1,6 +1,6 @@
 """
 This script is used to move annotated images from the 'not_yet_annotated' folder to the 'annotated' one. Their
-corresponding .xml annotations are also moved to the 'annotated' folder.
+corresponding annotation files are also moved to the 'annotated' folder.
 """
 import glob
 import os
@@ -8,15 +8,35 @@ import shutil
 import paths
 
 
-def is_annotated(image_file):
+def is_annotated(image_file, extension="json"):
     """
     Returns True or False, depending on whether the image has a corresponding .xml file in the same directory where it
     is located.
-    :param image_name: the full path name of the image file. The function checks for an .xml file in the same directory
+    :param image_file: the full path name of the image file. The function checks for an annotation file in the same
+    directory, with the specified extension.
+    :param extension: the file type of the annotation.
     :return: True or False
     """
-    annot_file = image_file.split(sep=".")[0] + ".xml"
+    annot_file = image_file.split(sep=".")[0] + "." + extension
     return os.path.exists(annot_file)
+
+
+def move_instance(image_file, target_path, extension="json"):
+    """
+    Moves the training instance image_file with its corresponding annotation to the target_path
+
+    :param image_file: the full path name of the image file. The function checks for an annotation file in the same
+    directory, with the specified extension.
+    :param target_path: the path ehre the image file and the annotation file will be moved.
+    :param extension: the file type of the annotation.
+    :return: None
+    """
+    image_name = os.path.basename(image_file)
+    annot_file = image_file.split(sep=".")[0] + "." + extension
+    annot_name = image_name.split(sep=".")[0] + "." + extension
+    shutil.move(image_file, os.path.join(target_path, image_name))
+    shutil.move(annot_file, os.path.join(target_path, annot_name))
+    print("Moved: {}".format(image_name))
 
 
 if __name__ == '__main__':
@@ -26,9 +46,4 @@ if __name__ == '__main__':
 
     for image_file in glob.glob(REGEX_SOURCE_IMAGES):
         if is_annotated(image_file):
-            image_name = os.path.basename(image_file)
-            annot_file = image_file.split(sep=".")[0] + ".xml"
-            annot_name = image_name.split(sep=".")[0] + ".xml"
-            shutil.move(image_file, os.path.join(TARGET_PATH, image_name))
-            shutil.move(annot_file, os.path.join(TARGET_PATH, annot_name))
-            print("Moved: {}".format(image_name))
+            move_instance(image_file, TARGET_PATH)
