040d7c0ec2115968ed4d168f5b2ae6ccc45e7c0c
Paul Féry <p.h.fery-1@student.tudelft.nl>
2022-05-11 18:30:11 +0200

    DataLoader class ready for use.

--- code/datasets/data_generator.py
+++ code/datasets/data_generator.py
@@ -1,32 +1,44 @@
+import json
 import torch
 import glob
-import xml.etree.ElementTree as ET
 import os
-from torchvision import transforms
+import paths
+import torchvision
+from PIL import Image
 
-class ASLDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_path, transforms=None):
+
+class HandCommandsDataset(torch.utils.data.Dataset):
+
+    def __init__(self, dataset_path, extension="json"):
         self.root_path = dataset_path
-        self.files = glob.glob(self.root_path+"/*.xml")
-        self.transforms = transforms
-    def __len__(self):
-        return len(self.files)
+        self.format = extension
+        self.tensor_converter = torchvision.transforms.ToTensor()
+        assert self.__len__() == len(glob.glob(os.path.join(self.root_path, "*.jpg"))),\
+            "Incomplete input/output pairs, check Dataset Folder:\n{}".format(self.root_path)
+
     def __getitem__(self, idx):
-        xml_path = os.path.join(self.root_path, self.files[idx])
-        root = ET.parse(xml_path).getroot()
-        xmin = int(root.find("object").find("bndbox").find("xmin").text)
-        ymin = int(root.find("object").find("bndbox").find("ymin").text)
-        xmax = int(root.find("object").find("bndbox").find("xmax").text)
-        ymax = int(root.find("object").find("bndbox").find("ymax").text)
-        class_name = root.find("object").find("name").text.lower()
-        if "spa" in class_name:
-            class_index = 0
-        elif "del" in class_name:
-            class_index = 1
-        else:
-            class_index = string.printable.index(class_name)-8
-        img = Image.open(os.path.join(self.root_path, root.find("filename").text))
-        if transforms:
-            img = self.transforms(img)
-        anotation = {"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,"class_name":class_name,"class_index":class_index, "file_path":os.path.join(self.root_path, root.find("filename").text)}
-        return img, anotation
+        annot_file = sorted(glob.glob(os.path.join(self.root_path, "*." + self.format)))[idx]
+        print(annot_file)
+        image_file = annot_file.split(".")[0] + ".jpg"
+        print(image_file)
+        with open(annot_file) as f:
+            annot_dict = json.load(f)
+        annot_dict = annot_dict[0]["annotations"]
+        print(annot_dict)
+        img = Image.open(image_file)
+        img = self.tensor_converter(img)
+        return img, annot_dict
+
+    def __len__(self):
+        return len(glob.glob(os.path.join(self.root_path, "*." + self.format)))
+
+
+if __name__ == '__main__':
+
+    DATA_PATH = os.path.join(paths.DATA_PATH, "annotated")
+    dataset = HandCommandsDataset(DATA_PATH)
+    index = 4
+
+    image, dictionary = dataset.__getitem__(index)
+    print(json.dumps(dictionary, indent=4))
+    print(image)
