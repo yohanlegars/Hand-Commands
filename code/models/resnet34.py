f686b4004d86ce1222de1e796ae0ec0204f53e83
matti <mattilang17@gmail.com>
2022-05-19 09:12:15 +0200

    Added resnet34 model

---
+++ code/models/resnet34.py
@@ -0,0 +1,24 @@
+"""
+Standard ResNet34 model taken from: https://towardsdatascience.com/bounding-box-prediction-from-scratch-using-pytorch-a8525da51ddc
+"""
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+class BB_model(nn.Module):
+    def __init__(self):
+        super(BB_model, self).__init__()
+        resnet = models.resnet34(pretrained=True)
+        layers = list(resnet.children())[:8]
+        self.features1 = nn.Sequential(*layers[:6])
+        self.features2 = nn.Sequential(*layers[6:])
+        self.classifier = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+        self.bb = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+
+    def forward(self, x):
+        x = self.features1(x)
+        x = self.features2(x)
+        x = F.relu(x)
+        x = nn.AdaptiveAvgPool2d((1, 1))(x)
+        x = x.view(x.shape[0], -1)
+        return self.classifier(x), self.bb(x)
