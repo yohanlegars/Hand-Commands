98129f32ee8618f1789d5c8453bab8ef6de69347
Paul Féry <p.h.fery-1@student.tudelft.nl>
2022-05-23 14:45:46 +0200

    Bit of work on training loop

--- code/training/train.py
+++ code/training/train.py
@@ -6,6 +6,8 @@ import data_generator
 import paths
 
 
+# partly inspired by: https://towardsdatascience.com/bounding-box-prediction-from-scratch-using-pytorch-a8525da51ddc
+
 def generate_dataset_splits(dataset, splitratio):
     split_one_amount = int(splitratio * len(dataset))
     split_two_amount = len(dataset) - split_one_amount
@@ -19,32 +21,30 @@ class Trainer(object):
     def __init__(self,
                  train_dataset: torch.utils.data.Dataset,
                  test_dataset: torch.utils.data.Dataset,
-                 classes: list):
-
+                 classes: list,
+                 batch_size: int,
+                 model: torch.nn.Module):
+        self.batch_size = batch_size
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
+        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.test_dataloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
         self.classes = classes
+        self.model = model
 
-    def train_one_epoch(self, epoch_index):
-        running_loss = 0.
-        last_loss = 0.
+    def train_single_epoch(self, epoch_index):
+        self.model.train()
+        total = 0
+        sum_loss = 0
+        for img, label_tensor, label, instance_name in self.train_dataloader:
+            batch = label.shape[0]
+            img = img.cuda().float()
+            label = label.cuda()
+            label_tensor = label_tensor.cuda().float()
+            out_class, out_pred = self.model(img)
 
-        for idx, data in enumerate(self.data_loader):
+            loss_class = torch.
 
-            # retrieve the image and annotation dictionary from the training instance
-            img, annot_dict = data
-
-            # zero the gradients for the batch
-            self.optimizer.zero_grad()
-
-            # prediction / forward pass     # TODO: see how it works with formatting of output
-            outputs = self.model(img)
-
-            # compute loss and gradients
-            loss = self.loss_fn(outputs, annot_dict)
-
-            # parameter update
-            self.optimizer.step()
 
 
 if __name__ == '__main__':
@@ -62,4 +62,5 @@ if __name__ == '__main__':
 
     trainer = Trainer(train_dataset=train_split,
                       test_dataset=test_split,
-                      classes=options.LABELS)
+                      classes=options.LABELS,
+                      batch_size=5)
--- code/utils/losses.py
+++ code/utils/losses.py
@@ -1,6 +1,36 @@
-import torch
+"""
 
-#### CLASSIFICATION LOSSES ####
+This section describes and organises the potential loss functions we are considering for the task at hand.
+
+
+For Label Classification:
+Let N be the number of different labels, and B the Batch Size. then 'input' and 'target' are defined by:
+
+    - 'input': a [B, N] tensor, each row containing the probability of belonging to class 'n'
+                    (or an unnormalized representation of that probability, ie a score).
+        For example, with N = 3 and B = 2, 'input' could be:
+                [[0.3, 0.4, 0.3],
+                 [0.2, 0.7, 0.1]]
+        Or, with just unnormalized scores:
+                [[4.25, -12.63, 3.25],
+                 [-6.22, 14.44, 13.69]]
+
+    - 'target': a [B, N] one hot encoded tensor, encoding the value of the label
+        For example, with N = 3 and B = 2, 'target' could be:
+                [[0.0, 1.0, 0.0],
+                 [0.0, 0.0, 1.0]]
+
+The potential loss functions considered here are:
+    - Cross Entropy: https://pytorch.org/docs/stable/generated/torch.nn.functional.cross_entropy.html#torch.nn.functional.cross_entropy
+    - Binary Cross Entropy: https://pytorch.org/docs/stable/generated/torch.nn.functional.binary_cross_entropy.html#torch.nn.functional.binary_cross_entropy
+
+
+For Regression / Localization of Sign:
+
+
+"""
+
+import torch
 
 # Example of target with class indices
 loss = torch.nn.CrossEntropyLoss()
@@ -23,5 +53,30 @@ output.backward()
 print(f"{input_tensor=}")
 print(f"{target=}")
 print(f"{output=}")
+
+
+if __name__ == '__main__':
+    """
+    An example of loss comparisons between random inputs and outputs,
+    with shape and format that will be used during regular training.
+    """
+    B = 2       # Batch Size
+    N = 5       # Number of labels
+
+    class_input = torch.randn([B, N], requires_grad=True)
+    class_input_softmaxed = torch.softmax(class_input, dim=1)
+
+    class_target = torch.empty(B, dtype=torch.long).random_(N)
+    class_target = torch.nn.functional.one_hot(class_target, num_classes=N)
+
+    print(f"{class_input=}")
+    print(f"{class_input_softmaxed=}")
+    print(f"{class_target=}")
+
+
+
+
+
+
 
 
