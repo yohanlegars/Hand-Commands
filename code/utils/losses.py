8ebf033a4678f41b4fb5ea98d54d892955dc1a89
Paul Féry <p.h.fery-1@student.tudelft.nl>
2022-05-24 18:53:39 +0200

    working on loss functions

--- code/utils/losses.py
+++ code/utils/losses.py
@@ -6,14 +6,10 @@ This section describes and organises the potential loss functions we are conside
 For Label Classification:
 Let N be the number of different labels, and B the Batch Size. then 'input' and 'target' are defined by:
 
-    - 'input': a [B, N] tensor, each row containing the probability of belonging to class 'n'
-                    (or an unnormalized representation of that probability, ie a score).
+    - 'input': a [B, N] tensor, each row containing the probability of belonging to class 'n'.
         For example, with N = 3 and B = 2, 'input' could be:
                 [[0.3, 0.4, 0.3],
                  [0.2, 0.7, 0.1]]
-        Or, with just unnormalized scores:
-                [[4.25, -12.63, 3.25],
-                 [-6.22, 14.44, 13.69]]
 
     - 'target': a [B, N] one hot encoded tensor, encoding the value of the label
         For example, with N = 3 and B = 2, 'target' could be:
@@ -32,27 +28,27 @@ For Regression / Localization of Sign:
 
 import torch
 
-# Example of target with class indices
-loss = torch.nn.CrossEntropyLoss()
-input_tensor = torch.randn(3, 5, requires_grad=True)
-target = torch.empty(3, dtype=torch.long).random_(5)
-output = loss(input_tensor, target)
-
-print(f"{input_tensor=}")
-print(f"{target=}")
-print(f"{output=}")
-
-output.backward()
-
-# Example of target with class probabilities
-input_tensor = torch.randn(3, 5, requires_grad=True)
-target = torch.randn(3, 5).softmax(dim=1)
-output = loss(input_tensor, target)
-output.backward()
-
-print(f"{input_tensor=}")
-print(f"{target=}")
-print(f"{output=}")
+# # Example of target with class indices
+# loss = torch.nn.CrossEntropyLoss()
+# input_tensor = torch.randn(3, 5, requires_grad=True)
+# target = torch.empty(3, dtype=torch.long).random_(5)
+# output = loss(input_tensor, target)
+#
+# print(f"{input_tensor=}")
+# print(f"{target=}")
+# print(f"{output=}")
+#
+# output.backward()
+#
+# # Example of target with class probabilities
+# input_tensor = torch.randn(3, 5, requires_grad=True)
+# target = torch.randn(3, 5).softmax(dim=1)
+# output = loss(input_tensor, target)
+# output.backward()
+#
+# print(f"{input_tensor=}")
+# print(f"{target=}")
+# print(f"{output=}")
 
 
 if __name__ == '__main__':
@@ -60,23 +56,32 @@ if __name__ == '__main__':
     An example of loss comparisons between random inputs and outputs,
     with shape and format that will be used during regular training.
     """
-    B = 2       # Batch Size
-    N = 5       # Number of labels
+    B = 5       # Batch Size
+    N = 2       # Number of labels
 
-    class_input = torch.randn([B, N], requires_grad=True)
-    class_input_softmaxed = torch.softmax(class_input, dim=1)
+    print("SIMULATING OUTPUTS FOR CLASSIFICATION + LOSS\n\n")
 
     class_target = torch.empty(B, dtype=torch.long).random_(N)
     class_target = torch.nn.functional.one_hot(class_target, num_classes=N)
+    class_target = class_target.type(torch.float32)
 
-    print(f"{class_input=}")
-    print(f"{class_input_softmaxed=}")
-    print(f"{class_target=}")
+    class_input = torch.randn([B, N], requires_grad=True)
+    class_input = torch.softmax(class_input, dim=1)
+
+    # if you want to check the loss for a "perfect prediction"
+    # class_input = torch.tensor(class_target, requires_grad=True)
+
+    print(f"{class_input=}\n")
+    print(f"{class_target=}\n")
+
+    # CE_loss = torch.nn.functional.cross_entropy(class_input, class_target)
+    # print(f"{CE_loss=}")
+    # CE_loss.backward()
+
+    BCE_loss = torch.nn.functional.binary_cross_entropy(class_input, class_target)
+    BCE_loss.backward()
+    print(f"{BCE_loss=}\n\n\n")
 
 
-
-
-
-
-
+    print("SIMULATING OUTPUTS FOR REGRESSION + LOSS\n\n")
 
