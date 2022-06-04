56d5a0295ee5ff47a7027fd02b9b229a1a778eeb
Yohan Le Gars <y.j.p.legars@student.tudelft.nl>
2022-06-04 14:31:42 +0200

    randomHorizontalflip

--- code/utils/data_augment.py
+++ code/utils/data_augment.py
@@ -19,40 +19,27 @@ import torchvision.transforms as T
 ########################################################################################################################
 # WIP ZONE: YOHAN
 
-class RandomHorizontalFlip(torch.nn.Module):
-    """
-    Horizontally flip the given image randomly with a given probability.
-    Args:
-        p (float): probability of the image being flipped. Default  value is 0.5
-    """
-    def __init__(self, p=1):
-        super().__init__()
-        self.p = p
 
-    def forward(self, dataset, num):
-        image, coord_tensor, label_tensor, _ = dataset.__getitem__(num)
-        transform = T.RandomHorizontalFlip(self.p)
+
+
+def Random_Horizontal_Flip(dataset, num):
+    image, coord_tensor, label_tensor, _ = dataset.__getitem__(num)
+    k = random.randint(0,1)
+    p = 1
+
+    if k == 1:
+        transform = T.RandomHorizontalFlip(p)
         image_aug = transform(image)
-        if image_aug != image:
-            coord_tensor[0] = image.shape[2] - coord_tensor[0]
-            if label_tensor == torch.Tensor([0., 0., 1., 0., 0.]):
-                label_tensor = torch.Tensor([0., 0, 0., 1., 0.])
+        coord_tensor[0] = image.shape[2] - coord_tensor[0]
+        if label_tensor[2] == 1.:
+            label_tensor = torch.Tensor([0., 0, 0., 1., 0.])
 
-            elif label_tensor == torch.Tensor([0., 0., 0., 1., 0.]):
-                label_tensor = torch.Tensor([0., 0., 1., 0., 0.])
+        elif label_tensor[3] == 1.:
+            label_tensor = torch.Tensor([0., 0., 1., 0., 0.])
+    else:
+        image_aug = image
 
-            else:
-                label_tensor = label_tensor
-
-        return image_aug, image, coord_tensor, label_tensor
-
-
-
-
-
-
-
-
+    return image_aug, image, coord_tensor, label_tensor
 
 
 ########################################################################################################################
@@ -98,7 +85,8 @@ if __name__ == '__main__':
     DATA_PATH = os.path.join(paths.DATA_PATH, "annotated")
 
     dataset = data_generator.HandCommandsDataset(dataset_path=DATA_PATH)
-    crop_resolution = (300, 400)
+    #crop_resolution = (300, 400)
+
 
     image, coord_tensor, label_tensor, name = dataset.__getitem__(4)
     print(f"{image=}")
@@ -109,17 +97,27 @@ if __name__ == '__main__':
     print(f"{label_tensor=}")
     print(f"{label_tensor.shape=}")
     print(f"{name=}")
-    print(f"{crop_resolution=}")
+   # print(f"{crop_resolution=}")
     print("")
 
-    orig_img = visualization.visualize_single_instance(image, coord_tensor, label_tensor, name, dataset.label_list)
-    plt.figure(name)
-    plt.imshow(orig_img.permute(1, 2, 0))
+    num = 2
+    image_aug, image, coord_tensor, label_tensor = Random_Horizontal_Flip(dataset, num)
+    images = [image_aug, image]
+    for i in range(0,len(images)):
+        plt.imshow(images[i].permute(1,2,0))
+        plt.show()
 
-    image, coord_tensor, label_tensor, name = random_crop(image, coord_tensor, label_tensor, name, crop_resolution)
 
-    crop_img = visualization.visualize_single_instance(image, coord_tensor, label_tensor, name, dataset.label_list)
-    plt.figure(name)
-    plt.imshow(crop_img.permute(1, 2, 0))
-    plt.show()
+
+
+    # orig_img = visualization.visualize_single_instance(image, coord_tensor, label_tensor, name, dataset.label_list)
+    # plt.figure(name)
+    # plt.imshow(orig_img.permute(1, 2, 0))
+    #
+    # image, coord_tensor, label_tensor, name = random_crop(image, coord_tensor, label_tensor, name, crop_resolution)
+    #
+    # crop_img = visualization.visualize_single_instance(image, coord_tensor, label_tensor, name, dataset.label_list)
+    # plt.figure(name)
+    # plt.imshow(crop_img.permute(1, 2, 0))
+    # plt.show()
 
