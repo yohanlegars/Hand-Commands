b2d4f0f0b35b3aebeb4eec6c4680d1772305f85b
Yohan Le Gars <y.j.p.legars@student.tudelft.nl>
2022-06-06 10:42:52 +0200

    update darknet

--- code/utils/util.py
+++ code/utils/util.py
@@ -1,11 +1,21 @@
 from __future__ import division
 
+import config
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F 
 from torch.autograd import Variable
 import numpy as np
 import cv2 
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import random
+import os
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
 
 
 def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
@@ -193,6 +203,42 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         return output
     except:
         return 0
+
+
+
+def letterbox_image(img, inp_dim):
+    '''resize image with unchanged aspect ratio using padding'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w/img_w, h/img_h))
+    new_h = int(img_h * min(w/img_w, h/img_h))
+    resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
+    
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+
+    canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
+    
+    return canvas
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network. 
+    
+    Returns a Variable 
+    """
+    img = (letterbox_image(img, (inp_dim, inp_dim)))
+    img = img[:,:,::-1].transpose((2,0,1)).copy()
+    img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+    return img
+
+def load_classes(namesfile):
+    fp = open(namesfile, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
+
+
+
+
 
 
 
