d7eb5ca94746733cd371006d18216fe0f392b79c
Yohan Le Gars <y.j.p.legars@student.tudelft.nl>
2022-06-06 21:39:39 +0200

    darknet eval works

--- code/evaluation/detector.py
+++ code/evaluation/detector.py
@@ -1,15 +1,16 @@
 from __future__ import division
+
 import time
 import torch 
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 import cv2 
-from code.utils.util import *
+from code.models.darknet import Darknet
 import argparse
 import os 
 import os.path as osp
-from code.models.darknet import Darknet
+from code.utils.util import *
 import pickle as pkl
 import pandas as pd
 import random
@@ -24,19 +25,19 @@ def arg_parse():
    
     parser.add_argument("--images", dest = 'images', help = 
                         "Image / Directory containing images to perform detection upon",
-                        default = "imgs", type = str)
+                        default = "../imgs", type = str)
     parser.add_argument("--det", dest = 'det', help = 
                         "Image / Directory to store detections to",
-                        default = "det", type = str)
+                        default = "../det", type = str)
     parser.add_argument("--bs", dest = "bs", help = "Batch size", default = 1)
     parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
     parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
     parser.add_argument("--cfg", dest = 'cfgfile', help = 
                         "Config file",
-                        default = "cfg/yolov3.cfg", type = str)
+                        default = "confs/yolov3.cfg", type = str)
     parser.add_argument("--weights", dest = 'weightsfile', help = 
                         "weightsfile",
-                        default = "yolov3.weights", type = str)
+                        default = "confs/yolov3.weights", type = str)
     parser.add_argument("--reso", dest = 'reso', help = 
                         "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
                         default = "416", type = str)
@@ -50,11 +51,10 @@ confidence = float(args.confidence)
 nms_thesh = float(args.nms_thresh)
 start = 0
 CUDA = torch.cuda.is_available()
-
-
+print(args.cfgfile)
 
 num_classes = 80
-classes = load_classes("data/coco.names")
+classes = load_classes("../data/coco.names")
 
 
 
@@ -178,7 +178,7 @@ for i in range(output.shape[0]):
     
 output_recast = time.time()
 class_load = time.time()
-colors = pkl.load(open("pallete", "rb"))
+colors = pkl.load(open("../pallete", "rb"))
 
 draw = time.time()
 
@@ -186,15 +186,18 @@ draw = time.time()
 def write(x, results):
     c1 = tuple(x[1:3].int())
     c2 = tuple(x[3:5].int())
+    d1 = (int(c1[0]), int(c1[1]))
+    d2 = (int(c2[0]), int(c2[1]))
     img = results[int(x[0])]
     cls = int(x[-1])
     color = random.choice(colors)
     label = "{0}".format(classes[cls])
-    cv2.rectangle(img, c1, c2,color, 1)
-    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+    cv2.rectangle(img, d1, d2, color, 1)
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-    cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    d2 = (int(c2[0]), int(c2[1]))
+    cv2.rectangle(img, d1, d2, color, -1)
+    cv2.putText(img, label, (int(c1[0]), int(c1[1] + t_size[1] + 4)), cv2.FONT_HERSHEY_PLAIN, 1, [225, 255, 255], 1)
     return img
 
 
